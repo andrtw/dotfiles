89443dcf1651b5acf5449abbbd347512515b4786
andrtw <andrea96.t@gmail.com>
2024-10-01 18:44:06 +0200

    Setup nvim servers with options

--- neovim/.config/nvim/lua/configs/lspconfig.lua
+++ neovim/.config/nvim/lua/configs/lspconfig.lua
@@ -3,18 +3,12 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
+local servers = { "pyright", "html", "cssls", "tsserver", "eslint" }
 
-local servers = {
-  pyright = {},
-  html = {},
-  cssls = {},
-  tsserver = {},
-  eslint = {},
-}
-
-for name, opts in pairs(servers) do
-  opts.on_attach = on_attach
-  opts.on_init = on_init
-  opts.capabilities = capabilities
-  lspconfig[name].setup(opts)
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
 end
