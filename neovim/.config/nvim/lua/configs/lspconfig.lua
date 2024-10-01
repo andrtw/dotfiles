45c92657ab265c5b01e02e3a9b32f6c4ae5c534c
andrtw <andrea96.t@gmail.com>
2024-10-01 18:47:16 +0200

    Replace deprecated tsserver with ts_ls

--- neovim/.config/nvim/lua/configs/lspconfig.lua
+++ neovim/.config/nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright", "html", "cssls", "tsserver", "eslint" }
+local servers = { "pyright", "html", "cssls", "ts_ls", "eslint" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
