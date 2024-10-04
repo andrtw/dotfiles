473999aca6bd88e3f8d7e8f70c6d7d4ce14f4af2
andrtw <andrea96.t@gmail.com>
2024-10-04 12:12:44 +0200

    Add Harpoon plugin

--- neovim/.config/nvim/lua/mappings.lua
+++ neovim/.config/nvim/lua/mappings.lua
@@ -1,5 +1,35 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+local nomap = vim.keymap.del
+local harpoon = require "harpoon"
 
 map("i", "jk", "<ESC>")
+nomap("n", "<leader>h")
+
+-- Harpoon
+map("n", "<leader>ha", function()
+  harpoon:list():add()
+end)
+map("n", "<leader>hh", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+map("n", "<C-h>", function()
+  harpoon:list():select(1)
+end)
+map("n", "<C-j>", function()
+  harpoon:list():select(2)
+end)
+map("n", "<C-k>", function()
+  harpoon:list():select(3)
+end)
+map("n", "<C-l>", function()
+  harpoon:list():select(4)
+end)
+-- Toggle previous & next buffers stored within Harpoon list
+map("n", "<C-S-P>", function()
+  harpoon:list():prev()
+end)
+map("n", "<C-S-N>", function()
+  harpoon:list():next()
+end)
--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -43,4 +43,13 @@ return {
       },
     },
   },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    config = function()
+      require "configs.harpoon"
+    end,
+  },
 }
